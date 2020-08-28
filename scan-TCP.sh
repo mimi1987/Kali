dbaa82b9ac8504710724fa238a431e67beece459
Michael M <m.migsch@gmail.com>
2020-08-28 16:34:34 -0400

    Update scan-TCP.sh

--- scan-TCP.sh
+++ scan-TCP.sh
@@ -2,9 +2,13 @@
 
 # Do a TCP scan on user based input for IP and PORT.
 
-echo "Please enter IP Address: "
+echo "Please enter starting IP Address: "
 
 read ip_address
+
+echo "Please enter the last octet of the last IP Address: "
+
+read ip_address_octet
 
 echo "Please enter PORT number: "
 
@@ -13,7 +17,7 @@ read port_number
 
 # Do the TCP scan.
 
-nmap -sT $ip_address -p $port_number > /dev/null -oG Scan-Results.txt
+nmap -sT $ip_address-$ip_address_octet -p $port_number > /dev/null -oG Scan-Results.txt
 
 
 # Output all open port for $port_number.
